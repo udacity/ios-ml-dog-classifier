b75cd404ffc0ec18e630c38f24bc906df19a500e
Jarrod Parkes <jarrod@udacity.com>
2017-06-06 17:49:44 -0700

    add basic ios scaffold

---
+++ DogBreedPrediction/DogBreedPrediction/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DogBreedPrediction
+//
+//  Created by Jarrod Parkes on 6/5/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - AppDelegate: UIResponder, UIApplicationDelegate
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ClassifyVC()
+        
+        return true
+    }
+}
