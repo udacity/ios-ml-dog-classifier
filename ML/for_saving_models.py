66c6093eea65333ef32c2247ae1ba4652c833774
alexisbcook <alexis.cook@gmail.com>
2017-06-10 01:24:28 +0200

    new student model

---
+++ ML/for_saving_models.py
@@ -0,0 +1,27 @@
+# for saving resnet-50 model
+# from keras.applications.resnet50 import ResNet50
+# model = ResNet50(weights='imagenet')
+# model.save('ResNet50_for_iOS.h5')
+
+# for saving student model
+from keras.applications.resnet50 import ResNet50
+from keras.models import load_model, Model
+from keras.layers import GlobalAveragePooling2D, Dropout, Dense
+
+resnet_50_model = ResNet50(weights='imagenet', include_top=False)
+last = resnet_50_model.output
+x = GlobalAveragePooling2D()(last)
+x = Dense(512, activation='relu')(x)
+x = Dropout(0.5)(x)
+preds = Dense(133, activation='softmax')(x)
+
+student_model = Model(resnet_50_model.input, preds)
+
+student_model_stump = load_model('student_model_stump_keras1.h5')
+student_model.layers[-4].set_weights(student_model_stump.layers[0].get_weights())
+student_model.layers[-3].set_weights(student_model_stump.layers[1].get_weights())
+student_model.layers[-2].set_weights(student_model_stump.layers[2].get_weights())
+student_model.layers[-1].set_weights(student_model_stump.layers[3].get_weights())
+
+student_model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+student_model.save('student_model_for_iOS.h5')
